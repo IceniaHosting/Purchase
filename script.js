f1ab19545cb271a44373fc9b37a3ec081a92ed82
IceniaHosting <fisharefriendsnotfoodjr@gmail.com>
2025-01-27 18:55:42 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function showForm() {
-    document.getElementById('signupForm').style.display = 'block';
-}
-
-function hideForm() {
-    document.getElementById('signupForm').style.display = 'none';
-}
